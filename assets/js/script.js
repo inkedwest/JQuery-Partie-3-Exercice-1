69d9b34842adfee514db6340badc9fa449b18724
Glatigny <f.west@live.fr>
2019-02-05 11:45:54 +0100

    exo 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 $(function(){
     $(document).ready(function(){
         $('#clickBtn').on('click', function(){ // Appel de la fonction au clic
-            var $test = $('#clickNmb').text(); 
-            $test++;
-            $('#clickNmb').text($test);
+            var $nombre = $('#clickNmb').text();
+            $nombre++;
+            $('#clickNmb').text($nombre);
         });
     });
 });
